c92bae251784882ff257cb2abcda68db0cfd1139
davisniranjan <119030112+davisniranjan@users.noreply.github.com>
2023-07-22 16:34:20 +0530

    js for e-commerce

--- main.js
+++ main.js
@@ -22,12 +22,10 @@ app.config(function ($routeProvider) {
 });
 
 app.controller('MainController', function ($scope) {
-  // Add any shared functionality for all pages here
+  // ...
 });
 
 app.controller('ProductsController', function ($scope) {
-  // Add product listing and functionality here
-  // Example:
   $scope.products = [
     { name: 'Product 1', price: 10.99 },
     { name: 'Product 2', price: 15.99 },
@@ -35,19 +33,18 @@ app.controller('ProductsController', function ($scope) {
   ];
 
   $scope.addToCart = function (product) {
-    // Implement logic to add products to the cart
+    // ...
   };
 });
 
 app.controller('CartController', function ($scope) {
-  // Implement cart functionality here
-  // Example:
+  
   $scope.cartItems = [
     { name: 'Product 1', price: 10.99, quantity: 2 },
     { name: 'Product 2', price: 15.99, quantity: 1 }
   ];
 
   $scope.removeItemFromCart = function (index) {
-    // Implement logic to remove items from the cart
+    //..
   };
 });
